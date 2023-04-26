bf8ec20432f04165100c4799d87440180dee84e9
Tanguy Merrien <tmerrien@outlook.com>
2023-04-26 10:48:05 -0300

    Add files via upload

---
+++ FindTheIndexOfTheFirstOccurrenceInAString/Program.cs
@@ -0,0 +1,43 @@
+﻿// Input
+string haystack = "leetcode", needle = "leeto";
+
+// Output
+int index = StrStr(haystack, needle);
+Console.WriteLine(index);
+
+// Algorithm
+int StrStr(string haystack, string needle)
+{
+    if (needle.Length == 0)
+    {
+        return 0;
+    }
+    if (haystack.Length == 0)
+    {
+        return -1;
+    }
+    for (int i = 0; i < haystack.Length; i++)
+    {
+        if (haystack[i] == needle[0])
+        {
+            if (haystack.Length - i < needle.Length)
+            {
+                return -1;
+            }
+            bool found = true;
+            for (int j = 0; j < needle.Length; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,42 @@
+﻿// Input
+int[] nums;
+int target;
+
+// Test case 1 
+nums = new int[] { 1, 3, 5, 6 };
+target = 5;
+Console.WriteLine(SearchInsert(nums, target));
+
+// Test case 2
+nums = new int[] { 1, 3, 5, 6 };
+target = 2;
+Console.WriteLine(SearchInsert(nums, target));
+
+// Test case 3
+nums = new int[] { 1, 3, 5, 6 };
+target = 7;
+Console.WriteLine(SearchInsert(nums, target));
+
+// Algorithm
+int SearchInsert(int[] nums, int target)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+    while (left <= right)
+    {
+        
+        int mid = left + (right - left) / 2;
+        switch (nums[mid].CompareTo(target))
+        {
+            case 0:
+                return mid;
+            case -1:
+                left = mid + 1;
+                break;
+            case 1:
+                right = mid - 1;
+                break;
+        }
+    }
+    return left;
+}
