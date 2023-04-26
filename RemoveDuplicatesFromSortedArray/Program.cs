16f5711cbe2ee8120efd290cb9d403097a8ee416
Tanguy Merrien <tmerrien@outlook.com>
2023-04-26 10:49:05 -0300

    Add files via upload
    RemoveElement
    RemoveDuplicatesFromSortedArray

---
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RemoveDuplicates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+        Console.WriteLine(RemoveDuplicates(nums));
+    }
+
+    public static int RemoveDuplicates(int[] nums)
+    {
+        List<int> finalNum = new List<int>();
+        foreach (int i in nums)
+        {
+            if (!finalNum.Contains(i))
+            {
+                finalNum.Add(i);
+            }
+        }
+        for (int i = 0; i < finalNum.Count; i++)
+        {
+            nums[i] = finalNum[i];
+        }
+        return finalNum.Count;
+    }
+}
---
+++ RemoveElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int[] nums;
+int val;
+
+nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+val = 2;
+
+Console.WriteLine(RemoveElement(nums, val));
+
+int RemoveElement(int[] nums, int val)
+{
+    int i = 0;
+    for (int j = 0; j < nums.Length; j++)
+    {
+        if (nums[j] != val)
+        {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
